5dfe709f191ee90e92941d84c9ed22df845027fd
meryem08 <meriem.mansouri08@gmail.com>
2023-09-29 01:45:47 +0100

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
 const init = () => {
+    const inputForm = document.querySelector('form');
   
-}
-
-document.addEventListener('DOMContentLoaded', init);
+    inputForm.addEventListener('submit', (event) => {
+      event.preventDefault();
+      const input = document.querySelector('input#searchByID');
+  
+      fetch(`http://localhost:3000/movies/${input.value}`)
+        .then((response) => response.json())
+        .then((data) => {
+          const titleElement = document.querySelector('section#movieDetails h4');
+          const summaryElement = document.querySelector('section#movieDetails p');
+  
+          titleElement.innerText = data.title;
+          summaryElement.innerText = data.summary;
+        });
+    });
+  };
+  
+  document.addEventListener('DOMContentLoaded', init);
+ 
